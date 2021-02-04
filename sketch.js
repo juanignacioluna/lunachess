abf819b25c545dcfb73d84faaebc887faedd938d
Juan Ignacio Luna <chinoluna11@gmail.com>
2021-02-04 11:43:19 -0300

    creando el tablero

--- sketch.js
+++ sketch.js
@@ -1,15 +1,82 @@
 
+let tablero
 
+let anchoCasilla = 60
+
+let anchoCanvas = anchoCasilla * 8
 
 function setup() {
 
-	createCanvas(300, 300)
+	createCanvas(anchoCanvas, anchoCanvas)
 
-	background(131, 78, 148)
+	background(255, 255, 255)
+
+	tablero = new Array(8)
+		  
+
+	for (let i = 0; i < tablero.length; i++) { 
+		tablero[i] = new Array(8)
+	} 
+
+
+
+	let y = 0
+
+	for (let i = 0; i < 8; i++) { 
+
+		let x = 0
+
+		for (let j = 0; j < 8; j++) { 
+
+			let claro = ((j+i) % 2 == 0)
+
+			tablero[i][j] = new Casilla(x,y,anchoCasilla, claro)
+
+			x+=anchoCasilla
+
+		} 
+
+		y+=anchoCasilla
+	}  
 
 }
 
 function draw() {
 
+	for (let i = 0; i < 8; i++) {
+		for (let j = 0; j < 8; j++) {
+
+			tablero[i][j].mostrar()
+
+		}
+	}
+
+}
+
+
+function Casilla(x,y,ancho, claro){
+
+	this.ancho = ancho
+
+	this.x = x
+
+	this.y = y
+
+	this.claro = claro
+
+	this.mostrar = () => {
+
+		if(this.claro){
+			fill(159, 144, 176)
+		}else{
+			fill(131, 78, 148)
+		}
+
+		noStroke()
+
+		rect(this.x, this.y, this.ancho, this.ancho)	
+
+
+	}
 
 }
